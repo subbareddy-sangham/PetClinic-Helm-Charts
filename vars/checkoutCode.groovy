b2174e671f1b78279c0e2bcfe98866b4d0c16f80
Subbu Reddy Sangham <165556941+Subbu2025@users.noreply.github.com>
2024-12-03 21:12:59 +0530

    Create checkoutCode.groovy

---
+++ vars/checkoutCode.groovy
@@ -0,0 +1,20 @@
+
+def call(Map params) {
+    assert params.containsKey('url') : "Missing required parameter: url"
+    assert params.containsKey('credentialsId') : "Missing required parameter: credentialsId"
+
+    String branch = params.get('branch', 'main')
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[
+            url: params.url,
+            credentialsId: params.credentialsId
+        ]]
+    ])
+    
+    // Retrieve the commit SHA after checkout
+    env.shortCommitSha = sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
+    echo "Retrieved Commit SHA: ${env.shortCommitSha}"
+}
