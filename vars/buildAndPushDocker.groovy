405ec80550038215e1d33d52743d36312c258041
Subbu Reddy Sangham <165556941+Subbu2025@users.noreply.github.com>
2024-12-09 00:13:49 +0530

    Update buildAndPushDocker.groovy

--- vars/buildAndPushDocker.groovy
+++ vars/buildAndPushDocker.groovy
@@ -1,18 +1,16 @@
-// vars/buildAndPushDocker.groovy
-def call(Map params) {
+def call(Map params = [:]) {
     assert params.containsKey('repoUrl') : "Missing required parameter: repoUrl"
     assert params.containsKey('awsCredentialsId') : "Missing required parameter: awsCredentialsId"
     assert params.containsKey('dockerImageName') : "Missing required parameter: dockerImageName"
-    assert params.containsKey('shortCommitSha') : "Missing required parameter: shortCommitSha"
 
     String repoUrl = params.repoUrl
     String awsCredentialsId = params.awsCredentialsId
     String dockerImageName = params.dockerImageName
-    String prId = params.prId
-    String shortCommitSha = params.shortCommitSha
+    String prId = params.prId ?: "default" // Optional fallback
+    String shortCommitSha = params.shortCommitSha ?: "latest" // Optional fallback
     String buildNumber = env.BUILD_NUMBER ?: '0'
 
-    // Updated ECR image tag to include the PR ID, commit SHA, and build number
+    // Updated ECR image tag
     String imageTag = "${prId}-${shortCommitSha}-build-${buildNumber}"
     String ecrImageTag = "${repoUrl}:${imageTag}"
 
@@ -20,9 +18,14 @@ def call(Map params) {
     sh "docker build -t ${ecrImageTag} ."
 
     // Push Docker Image to ECR
-    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: awsCredentialsId]]) {
-        sh "aws ecr get-login-password --region ${params.awsRegion} | docker login --username AWS --password-stdin ${repoUrl}"
-        sh "docker push ${ecrImageTag}"
+    withCredentials([[ 
+        $class: 'AmazonWebServicesCredentialsBinding', 
+        credentialsId: awsCredentialsId 
+    ]]) {
+        sh """
+        aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${repoUrl}
+        docker push ${ecrImageTag}
+        """
     }
 
     echo "Successfully built and pushed Docker image: ${ecrImageTag}"
